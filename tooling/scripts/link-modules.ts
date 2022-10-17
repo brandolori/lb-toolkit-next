845b73a2da9abfecba30101f1e6f0ad4b7f4c949
Lorenzo Bartolini <lbartolini@dynamicait.com>
2022-10-17 17:47:15 +0200

    improve linking

--- tooling/scripts/link-modules.ts
+++ tooling/scripts/link-modules.ts
@@ -5,11 +5,16 @@ import webpackPaths from '../configs/webpack.paths'
 const { srcNodeModulesPath } = webpackPaths
 const { appNodeModulesPath } = webpackPaths
 
-const devbuildsNodeModulesPath = join(__dirname, "..\\devbuilds\\node_modules")
+const devBuildsPath = join(__dirname, "..\\devbuilds")
+
+const devbuildsNodeModulesPath = join(devBuildsPath, "node_modules")
 
 if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
     fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction')
 }
 if (!fs.existsSync(devbuildsNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
+    if (!fs.existsSync(devBuildsPath)) {
+        fs.mkdirSync(devBuildsPath)
+    }
     fs.symlinkSync(appNodeModulesPath, devbuildsNodeModulesPath, 'junction')
 }
